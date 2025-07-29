77897aadb68d5255f7b664082a1c01f972f0c5b3
Kathir-R <harikathir2004@gmail.com>
2025-07-29 10:35:48 +0530

    Refactor joystick handling and update game initialization logic

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/painting.dart';
 import 'package:pixel_adventure/actors/player.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
@@ -14,6 +14,7 @@ class PixelAdventure extends FlameGame
   late final CameraComponent cam;
   Player player = Player(character: 'Mask Dude');
   late JoystickComponent joystick;
+  bool showJoystick = true;
 
   @override
   FutureOr<void> onLoad() async {
@@ -24,19 +25,55 @@ class PixelAdventure extends FlameGame
 
     cam = CameraComponent.withFixedResolution(
       world: world,
-      width: 640,
+      width: 600,
       height: 360,
     );
     cam.viewfinder.anchor = Anchor.topLeft;
 
     addAll([cam, world]);
 
-    addJoyStick();
+    if (showJoystick) {
+      addJoyStick();
+    }
 
     return super.onLoad();
   }
 
+  @override
+  void update(double dt) {
+    if (showJoystick) {
+      updateJoystick();
+    }
+    super.update(dt);
+  }
+
   void addJoyStick() {
-    joystick = JoystickComponent();
+    joystick = JoystickComponent(
+      knob: SpriteComponent(sprite: Sprite(images.fromCache('HUD/Knob.png'))),
+
+      background: SpriteComponent(
+        sprite: Sprite(images.fromCache('HUD/Joystick.png')),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+      default:
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
   }
 }
