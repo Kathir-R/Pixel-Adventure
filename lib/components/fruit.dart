04a82b4d66be947ea241145969aaf51249ceaa93
Kathir-R <harikathir2004@gmail.com>
2025-08-06 17:21:18 +0530

    Fixed Bugs in Fruit dart file

--- lib/components/fruit.dart
+++ lib/components/fruit.dart
@@ -8,23 +8,11 @@ import 'package:pixel_adventure/pixel_adventure.dart';
 class Fruit extends SpriteAnimationComponent
     with HasGameReference<PixelAdventure>, CollisionCallbacks {
   final String fruit;
-  Fruit({
-    this.fruit = 'Apple',
-    position,
-    size,
-  }) : super(
-          position: position,
-          size: size,
-        );
+  Fruit({this.fruit = 'Apple', position, size})
+    : super(position: position, size: size);
 
-  bool _collected = false;
   final double stepTime = 0.05;
-  final hitbox = CustomHitbox(
-    offsetX: 10,
-    offsetY: 10,
-    width: 12,
-    height: 12,
-  );
+  final hitbox = CustomHitbox(offsetX: 10, offsetY: 10, width: 12, height: 12);
 
   @override
   FutureOr<void> onLoad() {
@@ -49,22 +37,18 @@ class Fruit extends SpriteAnimationComponent
     return super.onLoad();
   }
 
-  void collidedWithPlayer() {
-    if (!_collected) {
-      animation = SpriteAnimation.fromFrameData(
-        game.images.fromCache('Items/Fruits/Collected.png'),
-        SpriteAnimationData.sequenced(
-          amount: 6,
-          stepTime: stepTime,
-          textureSize: Vector2.all(32),
-          loop: false,
-        ),
-      );
-      _collected = true;
-    }
-    Future.delayed(
-      const Duration(milliseconds: 400),
-      () => removeFromParent(),
+  void collidedWithPlayer() async {
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('Items/Fruits/Collected.png'),
+      SpriteAnimationData.sequenced(
+        amount: 6,
+        stepTime: stepTime,
+        textureSize: Vector2.all(32),
+        loop: false,
+      ),
     );
+
+    await animationTicker?.completed;
+    removeFromParent();
   }
 }
